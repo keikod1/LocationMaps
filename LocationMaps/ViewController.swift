883e580a618afe504a491c2ed4d8af1bbd980d25
kiku <madragimova@icloud.com>
2022-03-26 19:54:29 +0600

    Initial Commit

---
+++ LocationMaps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocationMaps
+//
+//  Created by diwka on 26/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
